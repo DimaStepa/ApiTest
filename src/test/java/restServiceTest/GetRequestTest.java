891d4c0bda607a90e6cdb6b76a7459871f6c9894
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-11-18 23:46:54 +0300

    upd get method

--- src/test/java/restServiceTest/GetRequestTest.java
+++ src/test/java/restServiceTest/GetRequestTest.java
@@ -4,16 +4,23 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.AbstractIterableAssert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class GetRequestTest {
 
-    private final String BASE_URI = "https://reqres.in/";
+    private final static String BASE_URI = "https://reqres.in/";
+    private final static String BASE_PATH = "/api/users?page=2";
 
     @Test(description = "использовать локальный мок для выполнения запроса GET")
     public void getResponce200statusMyMock() {
@@ -27,7 +34,7 @@ public class GetRequestTest {
     public void getListUsers() {
         given()
                 .baseUri(BASE_URI)
-                .basePath("/api/users?page=2")
+                .basePath(BASE_PATH)
                 .contentType(ContentType.JSON)
                 .when().get()
                 .then().statusCode(200)
@@ -39,11 +46,26 @@ public class GetRequestTest {
     public void getListEmails() {
         List<String> listOfEmails = given()
                 .baseUri(BASE_URI)
-                .basePath("/api/users?page=2")
+                .basePath(BASE_PATH)
                 .contentType(ContentType.JSON)
                 .when().get()
                 .then().statusCode(200)
                 .extract().jsonPath().getList("data.email");
         System.out.println(listOfEmails);
+    }
+
+    @Test(description = "Рассмотрение десерелиализации")
+    public void useDeserelization() {
+        List<UserPojo> listOfEmails = given()
+                .baseUri(BASE_URI)
+                .basePath(BASE_PATH)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then().statusCode(200)
+                .extract().jsonPath().getList("data", UserPojo.class);
+        assertEquals(listOfEmails.get(0).getEmail(), "george.bluth@reqres.in");
+
+//        assertThat(listOfEmails).extracting(UserPojo::getEmail).contains("george.bluth@reqres.in");
+
     }
 }
