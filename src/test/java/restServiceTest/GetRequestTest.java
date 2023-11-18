854980028798c46c4b4f362d6dade2177976b712
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-11-18 23:07:37 +0300

    upd get method

--- src/test/java/restServiceTest/GetRequestTest.java
+++ src/test/java/restServiceTest/GetRequestTest.java
@@ -2,10 +2,14 @@ package restServiceTest;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetRequestTest {
 
@@ -27,6 +31,19 @@ public class GetRequestTest {
                 .contentType(ContentType.JSON)
                 .when().get()
                 .then().statusCode(200)
-                .log().all();
+                .log().all()
+                .body("data[0].last_name", equalTo("Bluth"));
+    }
+
+    @Test(description = "Записать все email в список")
+    public void getListEmails() {
+        List<String> listOfEmails = given()
+                .baseUri(BASE_URI)
+                .basePath("/api/users?page=2")
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then().statusCode(200)
+                .extract().jsonPath().getList("data.email");
+        System.out.println(listOfEmails);
     }
 }
