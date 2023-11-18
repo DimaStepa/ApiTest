b4961cef159b43faeeb5fa3c06e1bac8794211ef
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-11-18 22:56:51 +0300

    upd get method

--- src/test/java/restServiceTest/GetRequestTest.java
+++ src/test/java/restServiceTest/GetRequestTest.java
@@ -1,17 +1,32 @@
 package restServiceTest;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class GetRequestTest {
 
-    @Test
-    public void getResponce200status() {
+    private final String BASE_URI = "https://reqres.in/";
+
+    @Test(description = "использовать локальный мок для выполнения запроса GET")
+    public void getResponce200statusMyMock() {
         Response response = RestAssured
                 .get("http://localhost:8084/api/v1/accounts/list")
                 .andReturn();
         response.prettyPrint();
     }
 
+    @Test(description = "Вызвать GET-запрос для https://reqres.in/")
+    public void getListUsers() {
+        given()
+                .baseUri(BASE_URI)
+                .basePath("/api/users?page=2")
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then().statusCode(200)
+                .log().all();
+    }
 }
